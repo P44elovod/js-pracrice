da89da495555e960c847d56645704a77b133ae2c
Evgeniy Kovtun <evgeniy.ko@onyxenterprises.com>
2018-10-03 14:46:49 +0300

    Initial commit

---
+++ var/script.js
@@ -0,0 +1,100 @@
+//Работа с переменными
+//#1
+//var num = 3;
+//alert(num);
+
+//#2
+//var a = 10;
+//var b = 2;
+//alert((a+b)+' '+(a-b)+' '+(a*b)+' '+(a/b));
+
+
+//#3
+//var c = 15;
+//var d = 2;
+//var result = c+d;
+//alert(result);
+
+//#4
+//var a = 10;
+//var b = 2;
+//var c = 5;
+//alert(a+b+c);
+
+//var a=17;
+//var b=10;
+//var c = a-b;
+//var d = 7;
+//var result = c+d;
+//alert(result);
+
+
+//Работа со строками
+//#6
+//var str = 'Привет, Мир!';
+//alert(str);
+
+//#7
+//var str1 = 'Привет, ';
+//var srt2 = 'Мир!';
+//alert(str1+srt2);
+
+//#8
+//var name = 'myName';
+//alert('Привет, ' + name + '!');
+
+//#9
+//var age = 99;
+//alert('Мне '+age+' лет!'')
+
+//Функция prompt
+//#10
+//alert('Ваше имя '+prompt('Ваше имя'));
+
+//#11
+//var num = prompt('число');
+//alert(num*num);
+
+//Обращение к символам строки
+//#12
+//var str = 'abcde';
+//alert(str[0] + ' ' + str[2] + ' ' + str[4]);
+
+//#13
+//var num = '12345';
+//var result = num[0]*num[2]*num[3]*num[4];
+
+//Практика
+//#14
+//var hour = 60*60;
+//var day = hour*24;
+//var month = day*30;
+
+//#15
+//var hour = 16;
+//var min = 15;
+//var sec = 15;
+//alert(hour+':'+min+':'+sec);
+
+//#16
+//var num = 2;
+//alert(num*num);
+
+//Работа с присваиванием и декрементами
+//#17
+//var num = 47;
+//num += 7;
+//num -= 18;
+//num *= 10;
+//num /= 15;
+//alert(num);
+
+//#18
+//var num = 10;
+//num = num++;
+//num = num++1;
+//num = num--;
+//alert(num);
+
+
+
