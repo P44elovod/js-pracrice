67990fbc7d2c0940bd41a46046e112021416af67
P44elovod <eug.kovtun@gmail.com>
2018-10-16 23:56:27 +0300

    logic-function

---
+++ logic-func/script.js
@@ -0,0 +1,29 @@
+    // Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false. Показать решение.
+    //
+    // function isBigger(numFirst, numSecond) {
+    //
+    // var flag = false;
+    // if (numFirst == numSecond) {
+    //     flag = true;
+    // }
+    //     return flag;
+    // }
+    // Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false. Показать решение.
+    //
+    // function isSummBiggerThanTen(numFirst, numSecond) {
+    //
+    // var flag = false;
+    // if ((numFirst+numSecond) > 10) {
+    //     flag = true;
+    // }
+    //     return flag;
+    // }
+    // Сделайте функцию, которая параметром принимает число и проверяет - отрицательное оно или нет. Если отрицательное - пусть функция вернет true, а если нет - false. Показать решение.
+    // function isNegative(num) {
+    //
+    // var flag = false;
+    // if (num < 0) {
+    //     flag = true;
+    // }
+    //     return flag;
+    // }
