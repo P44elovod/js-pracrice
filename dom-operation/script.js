d51ab0a175c253aaec578101f85c611c2304ff16
P44elovod <eug.kovtun@gmail.com>
2018-10-30 23:16:37 +0200

    Задачи на отработку изменений DOM элементов

--- dom-operation/script.js
+++ dom-operation/script.js
@@ -34,3 +34,89 @@ function buttonClickInputChangesAgain(elem){
 
 }
 
+function func(elem) {
+    elem.value = 'Ку-ку!'
+
+}
+
+function onClickDisable(elem) {
+
+    elem.disabled = true;
+    elem.value = 'О, теперь меня больше не нажать!';
+
+}
+
+function onMouseOverChangeImage(elem) {
+
+    elem.src = "2.jpg";
+
+}
+function  onMouseOutChangeImage(elem){
+
+    elem.src = "1.jpg";
+
+
+}
+
+function buttonClickChangeColor() {
+
+    var input = document.getElementById('inp');
+    input.style.color = 'red';
+
+}
+
+function hide() {
+var elem = document.getElementById('hide');
+elem.style.display = 'none';
+}
+function show() {
+var elem = document.getElementById('hide');
+elem.style.display = 'inline';
+}
+
+function buttonClickChangeShapeAndColor() {
+
+    var input = document.getElementById('bigred')
+    input.style.color = 'red';
+    input.value = 'Ой, я поменял свой текст и css!';
+    input.style.borderRadius = '10px';
+    input.style.height = '55px';
+    input.style.width = '305px';
+
+}
+
+function onClickDisableNew() {
+
+    var input = document.getElementById('enable');
+    var inputEnableButton = document.getElementById('disable');
+
+    input.disabled = true;
+    input.value = 'О, теперь меня больше не нажать';
+    inputEnableButton.className = 'visible';
+
+}
+
+function onClickEnable() {
+
+    var input = document.getElementById('enable');
+    var inputEnableButton = document.getElementById('disable');
+
+    input.disabled = false;
+    input.value = 'О, меня снова можно нажимать!';
+    inputEnableButton.className = 'invisible';
+
+}
+
+function clickCount(elem) {
+
+
+    elem.value = Number(elem.value)+1;
+
+}
+
+function buttonClickChangeFloat() {
+
+
+
+}
+
